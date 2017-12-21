0c8d6fe11a6f99dbf5950ed13564eea5b1f1103a
Lázaro Nixon <lazaronixon@hotmail.com>
2017-12-21 18:34:23 -0200

    Simplify code

--- ios/RNTurbolinksManager.m
+++ ios/RNTurbolinksManager.m
@@ -9,39 +9,26 @@
 
 @implementation RNTurbolinksManager
 
-- (dispatch_queue_t)methodQueue
-{
+- (dispatch_queue_t)methodQueue {
     return dispatch_get_main_queue();
 }
 
 RCT_EXPORT_MODULE();
 
-- (UIView *)view
-{
+- (UIView *)view {
     _session = [[Session alloc] init];
     _session.delegate = self;
-    
     if(!_turbolinks){
         _turbolinks = [[RNTurbolinks alloc] initWithManager:self bridge:self.bridge];
     }
-    
-    [_turbolinks.navigationBar setTranslucent:YES];
-    
     return _turbolinks.view;
 }
 
-- (void)session:(Session *)session didFailRequestForVisitable:(id<Visitable>)visitable withError:(NSError *)error {
+- (void)session:(Session *)session didProposeVisitToURL:(NSURL *)URL withAction:(enum Action)action {
+    [self presentVisitableForSession:URL withAction:action];
 }
 
-- (void)session:(Session *)session didProposeVisitToURL:(NSURL *)URL withAction:(enum Action)action {
-    VisitableViewController *visitableViewController = [[VisitableViewController alloc] initWithUrl:URL];
-    if (action == ActionAdvance) {
-        [_turbolinks pushViewController:visitableViewController animated:YES];
-    } else if (action == ActionReplace) {
-        [_turbolinks popViewControllerAnimated:NO];
-        [_turbolinks pushViewController:visitableViewController animated:NO];
-    }
-    [_session visit:visitableViewController];
+- (void)session:(Session *)session didFailRequestForVisitable:(id<Visitable>)visitable withError:(NSError *)error {
 }
 
 - (void)sessionDidLoadWebView:(Session *)session {
@@ -60,11 +47,19 @@ RCT_EXPORT_MODULE();
     [UIApplication sharedApplication].networkActivityIndicatorVisible=NO;
 }
 
-RCT_CUSTOM_VIEW_PROPERTY(url, NSstring, RNTurbolinks)
-{
-    VisitableViewController *visitableViewController = [[VisitableViewController alloc] initWithUrl:[RCTConvert NSURL:json]];
-    [_turbolinks pushViewController:visitableViewController animated:YES];
+-(void) presentVisitableForSession:(NSURL *)URL withAction:(enum Action)action {
+    VisitableViewController *visitableViewController = [[VisitableViewController alloc] initWithUrl:URL];
+    if (action == ActionAdvance) {
+        [_turbolinks pushViewController:visitableViewController animated:YES];
+    } else if (action == ActionReplace) {
+        [_turbolinks popViewControllerAnimated:NO];
+        [_turbolinks pushViewController:visitableViewController animated:NO];
+    }
     [_session visit:visitableViewController];
+}
+
+RCT_CUSTOM_VIEW_PROPERTY(url, NSstring, RNTurbolinks) {
+    [self presentVisitableForSession:[RCTConvert NSURL:json] withAction:ActionAdvance];
 }
 
 @end
