80345faf7bc942eccaeacf37bceeb61fed22f3ce
Lázaro Nixon <lazaronixon@hotmail.com>
2017-12-21 00:09:36 -0200

    more work

--- Example/App.js
+++ Example/App.js
@@ -10,7 +10,7 @@ export default class App extends Component<{}> {
 
   render() {
     return (
-      <Turbolinks ref={(tl) => { this.turboLinksRef = tl; }}/>
+      <Turbolinks ref={tl => this.turboLinksRef = tl}/>
     );
   }
 }
