dcc9eb73c93fcb3e3cebd0fb9c79ecd204e8b79f
Lázaro Nixon <lazaronixon@hotmail.com>
2018-01-29 08:30:59 -0200

    Authenticationview android and ios

--- Example/components/AuthenticationView.android.js
+++ Example/components/AuthenticationView.android.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react'
-import { NavigatorIOS, ToolbarAndroid, Platform, WebView, View, StyleSheet } from 'react-native'
+import { ToolbarAndroid, Platform, WebView, View } from 'react-native'
 import Turbolinks from 'react-native-turbolinks'
 import Constants from './Constants';
 
 export default class AuthenticationView extends Component {
-
-  render() {
-    if (Platform.OS == 'ios') {
-      return ( <NavigatorIOS initialRoute={{component: MyScene, title: 'Sign in'}} style={{flex: 1}}/> );
-    } else {
-      return ( <MyScene style={{flex: 1}}/> );
-    }
-  }
-}
-
-class MyScene extends Component {
 
   state = { url: Constants.baseUrl + '/sign-in' }
 
@@ -26,17 +15,9 @@ class MyScene extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        {Platform.OS == 'android' && <ToolbarAndroid title="Sign in" style={styles.toolBar}/>}
+        <ToolbarAndroid title="Sign in" style={{height: 56, elevation: 4, backgroundColor: '#f5f5f5'}}/>
         <WebView source={{uri: this.state.url }} onNavigationStateChange={this.handleNavigationStateChange}/>
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  toolBar: {
-    height: 56,
-    elevation: 4,
-    backgroundColor: '#f5f5f5'
-  }
-})
---
+++ Example/components/AuthenticationView.ios.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { NavigatorIOS, Platform, WebView } from 'react-native'
+import Turbolinks from 'react-native-turbolinks'
+import Constants from './Constants';
+
+export default class AuthenticationView extends Component {
+
+  render() {
+    return (
+      <NavigatorIOS initialRoute={{component: MyScene, title: 'Sign in'}} style={{flex: 1}}/>
+    )
+  }
+}
+
+class MyScene extends Component {
+
+  state = { url: Constants.baseUrl + '/sign-in' }
+
+  handleNavigationStateChange = (navState) => {
+    if (this.state.url != navState.url) { Turbolinks.dismiss() }
+    this.setState({ url: navState.url })
+  }
+
+  render() {
+    return (
+        <WebView source={{uri: this.state.url }}
+                 onNavigationStateChange={this.handleNavigationStateChange}/>
+    )
+  }
+}
