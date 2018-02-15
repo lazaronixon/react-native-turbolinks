762b0de7ec46e87059d777f3f6c313ee47aa0b6f
Lázaro Nixon <lazaronixon@hotmail.com>
2018-02-15 13:11:58 -0200

    Fix alert to IOS

--- ios/TurbolinksUtils/NavigationController.swift
+++ ios/TurbolinksUtils/NavigationController.swift
@@ -15,10 +15,30 @@ class NavigationController: UINavigationController {
         
         self.session = Session(webViewConfiguration: webViewConfiguration)
         self.session.delegate = manager
+        self.session.webView.uiDelegate = self
         
         if (route != nil) {
             let tRoute = TurbolinksRoute(route!)
             self.tabBarItem = UITabBarItem(title: tRoute.tabTitle , image: tRoute.tabIcon, selectedImage: tRoute.tabIcon)
         }
+    }
+}
+
+extension NavigationController: WKUIDelegate {
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let bundle = Bundle(identifier: "com.apple.UIKit")!
+        let confirm = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let cancel = bundle.localizedString(forKey: "Cancel", value: nil, table: nil)
+        let ok = bundle.localizedString(forKey: "OK", value: nil, table: nil)
+        
+        confirm.addAction(UIAlertAction(title: cancel, style: .cancel) { (action) in
+            completionHandler(false)
+        })
+        
+        confirm.addAction(UIAlertAction(title: ok, style: .default) { (action) in
+            completionHandler(true)
+        })
+        
+        self.present(confirm, animated: true, completion: nil)
     }
 }
