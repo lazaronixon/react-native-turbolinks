a614caf835757d1b4cad1808bd6a25692a369f38
Lázaro Nixon <lazaronixon@hotmail.com>
2018-03-18 18:38:14 -0300

    Use semaphre instead while loop on ios

--- ios/TurbolinksUtils/TurbolinksSession.swift
+++ ios/TurbolinksUtils/TurbolinksSession.swift
@@ -20,22 +20,22 @@ class TurbolinksSession: Session {
     }
     
     fileprivate func cleanCookies() {
-        var isCleaningCookies = true
+        let semaphore = DispatchSemaphore(value: 1)
         let types = Set<String>([WKWebsiteDataTypeCookies])
         let date = Date(timeIntervalSince1970: 0)
-        let dataStore = webView.configuration.websiteDataStore
-        dataStore.removeData(ofTypes: types, modifiedSince: date) { isCleaningCookies = false }
-        while isCleaningCookies { RunLoop.main.run(mode: .defaultRunLoopMode, before: .distantFuture) }
+        let dataStore = self.webView.configuration.websiteDataStore
+        dataStore.removeData(ofTypes: types, modifiedSince: date) { semaphore.signal() }
+        semaphore.wait()
     }
     
     fileprivate func injectCookies() {
-        var isEvaluatingJavaScript = true
+        let semaphore = DispatchSemaphore(value: 1)
         guard let url = topmostVisitable?.visitableURL else { return }
         guard let sharedCookies = HTTPCookieStorage.shared.cookies(for: url) else { return }
         webViewCookie.loadHTMLString("<html><body></body></html>", baseURL: url)
         while webViewCookie.isLoading { RunLoop.main.run(mode: .defaultRunLoopMode, before: .distantFuture) }
-        webViewCookie.evaluateJavaScript(getJSCookie(sharedCookies)){ (r, e) in isEvaluatingJavaScript = false }
-        while isEvaluatingJavaScript { RunLoop.main.run(mode: .defaultRunLoopMode, before: .distantFuture) }
+        webViewCookie.evaluateJavaScript(getJSCookie(sharedCookies)){ (r, e) in semaphore.signal() }
+        semaphore.wait()
     }
     
     fileprivate func getJSCookie(_ cookies: [HTTPCookie]) -> String {
