c56c05eb458bb505ff6f4526f6e7ce0b1a1d271d
Lázaro Nixon <lazaronixon@hotmail.com>
2018-04-21 02:56:56 -0300

    Update ReactNative extensions android

--- android/src/main/java/com/lazaronixon/rnturbolinks/react/ReactAppCompatActivity.java
+++ android/src/main/java/com/lazaronixon/rnturbolinks/react/ReactAppCompatActivity.java
@@ -1,13 +1,9 @@
 package com.lazaronixon.rnturbolinks.react; /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
-
-
 
 import javax.annotation.Nullable;
 
@@ -80,8 +76,18 @@ public abstract class ReactAppCompatActivity extends AppCompatActivity
     }
 
     @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return mDelegate.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
         return mDelegate.onKeyUp(keyCode, event) || super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        return mDelegate.onKeyLongPress(keyCode, event) || super.onKeyLongPress(keyCode, event);
     }
 
     @Override
