9c98033d0cf7b751230e164968682b508d7b98d6
Lázaro Nixon <lazaronixon@hotmail.com>
2018-11-03 22:47:45 -0300

    cosmetic

--- scripts/postlink/android/postlink.js
+++ scripts/postlink/android/postlink.js
@@ -6,27 +6,19 @@ module.exports = () => {
   var manifestPath = path.join("android", "app", "src", "main", "AndroidManifest.xml");
 
   function setMinSdkVersion() {
-    if (buildGradlePath) {
-      var buildGradleContents = fs.readFileSync(buildGradlePath, "utf8");
-      var minSdkVersion = buildGradleContents.match(/minSdkVersion.+/)[0]
-      var minSdkVersionNew = `minSdkVersion = 19`;
-      buildGradleContents = buildGradleContents.replace(minSdkVersion, minSdkVersionNew)
-      fs.writeFileSync(buildGradlePath, buildGradleContents);
-    } else {
-        return Promise.reject('Error setting (minSdkVersion = 19)');
-    }
+    var buildGradleContents = fs.readFileSync(buildGradlePath, "utf8");
+    var minSdkVersion = buildGradleContents.match(/minSdkVersion.+/)[0]
+    var minSdkVersionNew = `minSdkVersion = 19`;
+    buildGradleContents = buildGradleContents.replace(minSdkVersion, minSdkVersionNew)
+    fs.writeFileSync(buildGradlePath, buildGradleContents);
   }
 
   function setAllowBackup() {
-    if (manifestPath) {
-      var manifestContents = fs.readFileSync(manifestPath, "utf8");
-      var androidAllowBackup = `android:allowBackup="false"`;
-      var androidAllowBackupNew = `android:allowBackup="true"`;
-      manifestContents = manifestContents.replace(androidAllowBackup, androidAllowBackupNew)
-      fs.writeFileSync(manifestPath, manifestContents);
-    } else {
-        return Promise.reject('Error setting (android:allowBackup="true")');
-    }
+    var manifestContents = fs.readFileSync(manifestPath, "utf8");
+    var androidAllowBackup = `android:allowBackup="false"`;
+    var androidAllowBackupNew = `android:allowBackup="true"`;
+    manifestContents = manifestContents.replace(androidAllowBackup, androidAllowBackupNew)
+    fs.writeFileSync(manifestPath, manifestContents);
   }
 
   setMinSdkVersion()
