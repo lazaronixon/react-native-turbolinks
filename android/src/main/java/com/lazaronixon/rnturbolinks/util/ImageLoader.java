36dd8d0951f4a10cda7c43392e95fc70f3f36edd
Lázaro Nixon <lazaronixon@hotmail.com>
2018-11-28 15:44:57 -0200

    resolve assets from project folder

--- android/src/main/java/com/lazaronixon/rnturbolinks/util/ImageLoader.java
+++ android/src/main/java/com/lazaronixon/rnturbolinks/util/ImageLoader.java
@@ -6,18 +6,38 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.StrictMode;
+import android.util.Log;
 
+import com.facebook.react.common.ReactConstants;
 import com.facebook.react.views.imagehelper.ResourceDrawableIdHelper;
+import com.lazaronixon.rnturbolinks.BuildConfig;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 public class ImageLoader {
+
     private static final String FILE_SCHEME = "file";
 
-    public static Drawable loadImage(Context context, String iconSource) {
-        Uri uri = Uri.parse(iconSource);
-        if (isLocalFile(uri)) {
-            return loadFile(context, uri);
-        } else {
-            return loadResource(context, iconSource);
+    public static Drawable loadImage(Context context, String source) {
+        Drawable drawable;
+
+        try {
+            if (isLocalFile(Uri.parse(source))) {
+                drawable = loadFile(context, source);
+            } else {
+                drawable = loadResource(context, source);
+                if (drawable == null || BuildConfig.DEBUG) {
+                    drawable = readJsDevImage(context, source);
+                }
+            }
+            return drawable;
+        } catch (IOException e) {
+            Log.e(ReactConstants.TAG, "Error loading image. " + e.toString());
+            return null;
         }
     }
 
@@ -25,13 +45,49 @@ public class ImageLoader {
         return FILE_SCHEME.equals(uri.getScheme());
     }
 
-    private static Drawable loadFile(Context context, Uri uri) {
-        Bitmap bitmap = BitmapFactory.decodeFile(uri.getPath());
+    private static Drawable loadFile(Context context, String uri) {
+        Bitmap bitmap = BitmapFactory.decodeFile(Uri.parse(uri).getPath());
         return new BitmapDrawable(context.getResources(), bitmap);
     }
 
     private static Drawable loadResource(Context context, String iconSource) {
         return ResourceDrawableIdHelper.getInstance().getResourceDrawable(context, iconSource);
     }
+
+    private static Drawable readJsDevImage(Context context, String source) throws IOException {
+        StrictMode.ThreadPolicy threadPolicy = adjustThreadPolicyDebug();
+        InputStream is = openStream(context, source);
+        Bitmap bitmap = BitmapFactory.decodeStream(is);
+        restoreThreadPolicyDebug(threadPolicy);
+        return new BitmapDrawable(context.getResources(), bitmap);
+    }
+
+    private static InputStream openStream(Context context, String uri) throws IOException {
+        return uri.contains("http") ? remoteUrl(uri) : localFile(context, uri);
+    }
+
+    private static InputStream remoteUrl(String uri) throws IOException {
+        return new URL(uri).openStream();
+    }
+
+    private static InputStream localFile(Context context, String uri) throws FileNotFoundException {
+        return context.getContentResolver().openInputStream(Uri.parse(uri));
+    }
+
+    private static StrictMode.ThreadPolicy adjustThreadPolicyDebug() {
+        StrictMode.ThreadPolicy threadPolicy = null;
+        if (BuildConfig.DEBUG) {
+            threadPolicy = StrictMode.getThreadPolicy();
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitNetwork().build());
+        }
+        return threadPolicy;
+    }
+
+    private static void restoreThreadPolicyDebug(StrictMode.ThreadPolicy threadPolicy) {
+        if (BuildConfig.DEBUG && threadPolicy != null) {
+            StrictMode.setThreadPolicy(threadPolicy);
+        }
+    }
+
 }
 
