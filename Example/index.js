7db529c867c8cd048f651fc44b19af34cd78979b
Lázaro Nixon <lazaronixon@hotmail.com>
2018-12-03 22:54:59 -0200

    cosmetic

--- Example/index.js
+++ Example/index.js
@@ -3,8 +3,9 @@ import App from './components/App'
 import NumbersView from './components/NumbersView'
 import ErrorView from './components/ErrorView'
 import AuthenticationView from './components/AuthenticationView'
+import {name as appName} from './app.json'
 
-AppRegistry.registerComponent('Example', () => App)
+AppRegistry.registerComponent(appName, () => App)
 AppRegistry.registerComponent('NumbersView', () => NumbersView)
 AppRegistry.registerComponent('ErrorView', () => ErrorView)
 AppRegistry.registerComponent('AuthenticationView', () => AuthenticationView)
