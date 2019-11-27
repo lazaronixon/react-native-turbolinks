143bd87cb30becb3f95aa1b6fc182569a54ee558
Lázaro Nixon <lazaronixon@hotmail.com>
2019-11-26 22:04:03 -0300

    revert authenticationView

--- Example/components/AuthenticationView.js
+++ Example/components/AuthenticationView.js
@@ -6,15 +6,11 @@ import { baseUrl } from './../app.json'
 
 const signInUrl = baseUrl + '/sign-in'
 const signedUrl = baseUrl + '/'
-var authenticatedFlag = false
 
 export default class AuthenticationView extends Component {
 
-  // authenticatedFlag prevents old android devices handleAuthentication twice.
   handleAuthentication = (event) => {
-    if (event.nativeEvent.url == signedUrl && authenticatedFlag == false) {
-      authenticatedFlag = true
-
+    if (event.nativeEvent.url == signedUrl) {
       this.webview.stopLoading()
       Turbolinks.dismiss().then(() => Turbolinks.reloadSession())
     }
