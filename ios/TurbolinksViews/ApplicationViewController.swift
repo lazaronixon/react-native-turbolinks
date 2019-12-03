bf57099354d2ebd1fefb9ac8e2f30a9b3bc790d9
Lázaro Nixon <lazaronixon@hotmail.com>
2019-12-03 16:42:22 -0300

    more refactor

--- ios/TurbolinksViews/ApplicationViewController.swift
+++ ios/TurbolinksViews/ApplicationViewController.swift
@@ -20,33 +20,30 @@ extension ApplicationViewController where Self: UIViewController {
     var isRoot: Bool { navigationController?.viewControllers.count == 1 }
     var _navigationItem: UINavigationItem { (self as UIViewController).navigationItem }
     var turbolinksBundle: Bundle { Bundle(path: Bundle.main.path(forResource: "RNTurbolinks", ofType: "bundle")!)! }
+    var defaultMenuIcon: UIImage { UIImage(named: "ic_menu", in: turbolinksBundle, compatibleWith: nil)! }
     
     func renderTitle() { _navigationItem.titleView = UITitleView(self) }
     
     func setupNavigationBar() { navigationController?.navigationBar.isHidden = route.navBarHidden }
     
     func renderBackButton() {
-        let backButton = UIBarButtonItem(title: nil, style: .plain, target: nil, action: nil)
-        _navigationItem.backBarButtonItem = backButton
+        _navigationItem.backBarButtonItem = UIBarButtonItem(title: nil, style: .plain, target: nil, action: nil)
         _navigationItem.leftItemsSupplementBackButton = true
     }
     
     func renderLeftButton() {
         guard let icon = route.leftButtonIcon else { return }
-        let button = UIBarButtonItem(image: icon, style: .plain, target: self, action: selectorHandleLeftButtonPress)
-        _navigationItem.leftBarButtonItem = button
+        _navigationItem.leftBarButtonItem = UIBarButtonItem(image: icon, style: .plain, target: self, action: selectorHandleLeftButtonPress)
     }
     
     func renderRightButton() {
         guard let icon = route.rightButtonIcon else { return }
-        let button = UIBarButtonItem(image: icon, style: .plain, target: self, action: selectorHandleRightButtonPress)
-        _navigationItem.rightBarButtonItem = button
+        _navigationItem.rightBarButtonItem = UIBarButtonItem(image: icon, style: .plain, target: self, action: selectorHandleRightButtonPress)
     }
     
     func renderActions() {
         guard let actions = route.actions, !actions.isEmpty else { return }
-        let icon = manager.menuIcon ?? UIImage(named: "ic_menu", in: turbolinksBundle, compatibleWith: nil)
-        _navigationItem.rightBarButtonItem = UIBarButtonItem(image: icon, style: .plain, target: self, action: selectorPresentActions)
+        _navigationItem.rightBarButtonItem = UIBarButtonItem(image: manager.menuIcon ?? defaultMenuIcon, style: .plain, target: self, action: selectorPresentActions)
     }
     
     func presentActions(_ sender: UIBarButtonItem) {
