1cb6f5a18d0d406d595b54cf132baa52030db08f
Lázaro Nixon <lazaronixon@hotmail.com>
2019-12-04 23:56:03 -0300

    extract primaryviewController

---
+++ ios/TurbolinksViews/PrimaryViewController.swift
@@ -0,0 +1,14 @@
+class PrimaryViewController: UIViewController {
+    var manager: RNTurbolinksManager!
+    var component: String!
+    
+    convenience required init(_ manager: RNTurbolinksManager,_ component: String) {
+        self.init()
+        self.manager = manager
+        self.component = component
+    }
+    
+    override func loadView() {
+        self.view = RCTRootView(bridge: manager.bridge, moduleName: component, initialProperties: nil)
+    }
+}
--- ios/TurbolinksViews/SplitViewController.swift
+++ ios/TurbolinksViews/SplitViewController.swift
@@ -8,18 +8,3 @@ class SplitViewController: UISplitViewController {
         self.viewControllers = [primaryController, secondaryController]
     }
 }
-
-class PrimaryViewController: UIViewController {
-    var manager: RNTurbolinksManager!
-    var component: String!
-    
-    convenience required init(_ manager: RNTurbolinksManager,_ component: String) {
-        self.init()
-        self.manager = manager
-        self.component = component
-    }
-    
-    override func loadView() {
-        self.view = RCTRootView(bridge: manager.bridge, moduleName: component, initialProperties: nil)
-    }
-}
